f1947736ae8bbe05c71a35084bb8e800e415a456
Maher Fayad <MaherFayad55@Gmail.com>
2023-09-06 18:18:24 +0300

    maherfayad

--- astro.config1234.mjs
+++ astro.config1234.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    // viewTransitions: true,
     optimizeHoistedScript: true,
-  },
+    },
   integrations: [tailwind(), compress()],
 });
