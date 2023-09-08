8f4f302ba1a81027218d4b1646eabb3f82876fe3
Maher Fayad <MaherFayad55@Gmail.com>
2023-09-08 21:38:20 +0300

    maher

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import react from "@astrojs/react";
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
     optimizeHoistedScript: true
   },
-  integrations: [tailwind(),sitemap(), robotsTxt(), react()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), react(), compress()]
 });
