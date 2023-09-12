cc5f60f4ffbb8f7e2d9964b08db83a3f21594d0c
Maher Fayad <MaherFayad55@Gmail.com>
2023-09-12 14:02:22 +0300

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import react from "@astrojs/react";
-import compress from "astro-compress";
 import partytown from '@astrojs/partytown';
 import purgecss from "astro-purgecss";
 
