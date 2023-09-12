f911efb6c5f5a1350032ce3a8e1870f595475733
Maher Fayad <MaherFayad55@Gmail.com>
2023-09-12 12:40:18 +0300

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import react from "@astrojs/react";
 import compress from "astro-compress";
+import htmlMinifier from "astro-html-minifier";
+import internaliseCss from "astro-internalise-css";
+import partytown from '@astrojs/partytown';
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     optimizeHoistedScript: true
-    
   },
-  integrations: [tailwind(), sitemap(), robotsTxt(), react()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), react(), htmlMinifier(), internaliseCss(), purgecss(), partytown()]
 });
