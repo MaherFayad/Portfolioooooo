d124dcbd7540e1ee834b95b97b7ed06ba96b807b
Maher Fayad <63962002+MaherFayad@users.noreply.github.com>
2023-09-25 13:40:24 +0200

    123

--- src/components/emailjs/emailjs.jsx
+++ src/components/emailjs/emailjs.jsx
@@ -103,5 +103,11 @@ export const ContactUs = () => {
         </div>
       </div>
     </section>
+    <script>
+      document.getElementById("Form").addEventListener("submit", function (e) {
+      e.preventDefault(); // Prevent the form from submitting traditionally
+      sendEmail(); // Call your sendEmail function
+    });
+    </script>
   );
 };
