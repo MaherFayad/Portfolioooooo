9a4da6874e944a28b7d974c1f413ca05946a3ee6
Maher Fayad <MaherFayad55@Gmail.com>
2023-10-13 19:42:54 +0300

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,15 @@ import partytown from '@astrojs/partytown';
 import svelte from '@astrojs/svelte';
 import alpinejs from "@astrojs/alpinejs";
 import compressor from "astro-compressor";
-
 import compress from "astro-compress";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     optimizeHoistedScript: true
   },
-  integrations: [tailwind(), sitemap(), robotsTxt(), react(), partytown(), svelte(), alpinejs(), compressor(), compress()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), react(), partytown(), svelte(), alpinejs(), compressor(), compress()],
+  output: "server",
+  adapter: vercel()
 });
