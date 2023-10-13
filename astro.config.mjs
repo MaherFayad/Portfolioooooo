bbc94e8740e9f0e33403a4df4add0fa9a0bbd12e
Maher Fayad <MaherFayad55@Gmail.com>
2023-10-13 19:25:17 +0300

    123

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from "@astrojs/react";
 import partytown from '@astrojs/partytown';
 import svelte from '@astrojs/svelte';
 import alpinejs from "@astrojs/alpinejs";
-
 import compressor from "astro-compressor";
 
 // https://astro.build/config
